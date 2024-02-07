ec46c67562a3ce484c8f779f07735cff201a4f98
kavindu Bandara <86820765+kavindu1082@users.noreply.github.com>
2024-02-07 12:55:49 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,29 @@ gsap.from('.nav__item', {opacity: 0, duration: 3, delay: 3.2, y: 25, ease:'expo.
 
 // SOCIAL
 gsap.from('.home__social-icon', {opacity: 0, duration: 3, delay: 4, y: 25, ease:'expo.out', stagger: .2})
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+// Update countdown time
+function updateCountdown() {
+	const currentTime = new Date();
+	const diff = newYearTime - currentTime;
+
+	const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+	const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+	const m = Math.floor(diff / 1000 / 60) % 60;
+	const s = Math.floor(diff / 1000) % 60;
+
+	days.innerHTML = d;
+	hours.innerHTML = h < 10 ? '0' + h : h;
+	minutes.innerHTML = m < 10 ? '0' + m : m;
+	seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+setInterval(updateCountdown, 1000);
